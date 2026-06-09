bb867e68b0ade64439452ae6ac6c119a366102f2
Kyler Davis <ky.s.dav@gmail.com>
2026-06-09 13:22:19 -0600

    updates

--- lib/features/auth/data/auth_service.dart
+++ lib/features/auth/data/auth_service.dart
@@ -7,32 +7,18 @@ class AuthService {
 
   User? get currentUser => _auth.currentUser;
 
-  Future<void> signUp({
-    required String email,
-    required String password,
-    required String displayName,
-  }) async {
-    final cred = await _auth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
+  Future<void> signUp({required String email, required String password, required String displayName}) async {
+    final cred = await _auth.createUserWithEmailAndPassword(email: email, password: password);
     await cred.user!.updateDisplayName(displayName);
   }
 
-  Future<void> signIn({
-    required String email,
-    required String password,
-  }) async {
+  Future<void> signIn({required String email, required String password}) async {
     await _auth.signInWithEmailAndPassword(email: email, password: password);
   }
 
   Future<void> signOut() => _auth.signOut();
 }
 
-final authServiceProvider = Provider<AuthService>(
-  (ref) => AuthService(FirebaseAuth.instance),
-);
+final authServiceProvider = Provider<AuthService>((ref) => AuthService(FirebaseAuth.instance));
 
-final currentUserProvider = StreamProvider<User?>(
-  (ref) => FirebaseAuth.instance.authStateChanges(),
-);
+final currentUserProvider = StreamProvider<User?>((ref) => FirebaseAuth.instance.authStateChanges());
